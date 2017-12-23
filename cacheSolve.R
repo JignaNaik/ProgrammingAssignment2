10c6d5fedfe3c45c8205b218e7657965ad5b1232
JignaNaik <34791167+JignaNaik@users.noreply.github.com>
2017-12-23 13:08:31 +0000

    Update and rename makeVector.R to cacheSolve.R

---
+++ cacheSolve.R
@@ -0,0 +1,13 @@
+## Returns m if already calculated and cached or calculates it
+cacheSolve <- function(x, ...) {
+        m <- x$getinverse()
+        if(!is.null(m)) {                    #checks whether m is already cached
+                message("getting cached data")
+                return(m)                   #returns m if already cached
+        }
+        data <- x$get()
+        m <- solve(data)                 #computes inverse of matrix
+        x$setinverse(m)
+        m                             # Returns inverse
+}
+
